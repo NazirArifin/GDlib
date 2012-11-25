e6ffa3842c2e04e2500bce11b8c200223ab17ef1
Dekiirawan <irawan.deki88@gmail.com>
2012-11-25 22:47:31 +0700

    edit human

---
+++ application/logs/log-2012-11-23.php
@@ -0,0 +1,11 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-11-23 18:37:59 --> 404 Page Not Found --> 
+ERROR - 2012-11-23 18:37:59 --> 404 Page Not Found --> 
+ERROR - 2012-11-23 18:37:59 --> 404 Page Not Found --> 
+ERROR - 2012-11-23 18:37:59 --> 404 Page Not Found --> 
+ERROR - 2012-11-23 18:37:59 --> 404 Page Not Found --> 
+ERROR - 2012-11-23 18:37:59 --> 404 Page Not Found --> 
+ERROR - 2012-11-23 18:37:59 --> 404 Page Not Found --> 
+ERROR - 2012-11-23 18:37:59 --> 404 Page Not Found --> 
+ERROR - 2012-11-23 18:37:59 --> 404 Page Not Found --> 
