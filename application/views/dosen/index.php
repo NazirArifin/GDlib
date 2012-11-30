f90e01854dc44bccc284faeb811658d9fd132d85
fajrul <fajrul.02.a7x@gmail.com>
2012-12-01 00:31:59 +0700

    Latihan-Dosen-index

--- application/views/dosen/index.php
+++ application/views/dosen/index.php
@@ -1,3 +1,74 @@
-<?php
-
-echo "halo";
+<!DOCTYPE html>
+<html lang="id">
+<head>
+	<meta charset="utf-8">
+	<title>Dosen | Beranda</title>
+	<link href="/third_party/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="/third_party/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
+	<style type="text/css">
+body {
+	font-size: 10px;
+}
+	</style>
+</head>
+<body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="./index.html">Bootstrap</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav pull-right">
+              <li class="active">
+                <a href="./index.html">Home</a>
+              </li>
+              <li class="">
+                <a href="./getting-started.html">Get started</a>
+              </li>
+              <li class="">
+                <a href="./scaffolding.html">Scaffolding</a>
+              </li>
+              <li class="">
+                <a href="./base-css.html">Base CSS</a>
+              </li>
+              <li class="">
+                <a href="./components.html">Components</a>
+              </li>
+              <li class="">
+                <a href="./javascript.html">JavaScript</a>
+              </li>
+              <li class="">
+                <a href="./customize.html">Customize</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+	
+	
+	
+	<div class="container">
+		<div class="row-fluid">
+			<div class="span2 bgblack">
+				
+				<div class="merah tebal">
+					<br><br><br><br><br><br><br><br><br><br><br><br><br><br>Habib 
+					<i class="icon-glass icon-white"></i>
+					<a href="#" class="btn btn-primary btn-mini pull-right"><i class="icon-off icon-white"></i> TOMBOL</a>
+				</div>
+			</div>
+			<div class="span10 well">
+				dfd
+			</div>
+		</div>
+	</div>
+	
+	<script type="text/javascript" src="/third_party/jquery/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="/third_party/bootstrap/bootstrap.min.js"></script>
+</body>
+</html>
