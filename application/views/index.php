f897c5994fad5ffef2746e94a505d6c3df82ee0a
rudiec <rudiec.nuada@gmail.com>
2012-12-04 00:11:10 +0700

    halaman depan lagi

--- application/views/index.php
+++ application/views/index.php
@@ -5,17 +5,27 @@
     <meta charset="utf-8" />
 		<title>GDlib | Home</title>
 			<link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
-			<link rel="stylesheet" href="third_party/css/smoothness/jquery-ui-1.9.1.custom.min.css" />
-			<link rel="stylesheet" href="third_party/bootstrap/css/bootstrap.min.css" />
-			<link rel="stylesheet" href="third_party/bootstrap/css/bootstrap-responsive.min.css" />
+			<link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
+			<link rel="stylesheet" type="text/css" href="third_party/bootmetro/content/css/bootstrap.css">
+			<link rel="stylesheet" type="text/css" href="third_party/bootmetro/content/css/bootstrap-responsive.css">
+			<link rel="stylesheet" type="text/css" href="third_party/bootmetro/content/css/bootmetro.css">
+			<link rel="stylesheet" type="text/css" href="third_party/bootmetro/content/css/bootmetro-tiles.css">
+			<link rel="stylesheet" type="text/css" href="third_party/bootmetro/content/css/bootmetro-charms.css">
+			<link rel="stylesheet" type="text/css" href="third_party/bootmetro/content/css/metro-ui-light.css">
+			<link rel="stylesheet" type="text/css" href="third_party/bootmetro/content/css/icomoon.css">
 			
 			<script src="third_party/jquery/jquery-1.8.2.min.js"></script>
 			<script src="third_party/jquery.ui/jquery-ui-1.9.1.custom.min.js"></script>
 			<script src="third_party/bootstrap/bootstrap.min.js"></script>
 			
+			<link href="third_party/slider/generic.css" rel="stylesheet" type="text/css" />
+			<link href="third_party/slider/2/js-image-slider.css" rel="stylesheet" type="text/css" />
+			<link href="third_party/slider/2/slider.css" rel="stylesheet" type="text/css" />
+			<script src="third_party/slider/jquery-1.7.1.min.js" type="text/javascript"></script>
+			<script src="third_party/slider/2/js-image-slider.js" type="text/javascript"></script>
+			<script src="third_party/slider/2/jquery-slider.js" type="text/javascript"></script>
    
-   
-    <style type="text/css">
+       <style type="text/css">
 
 			
 			#logo{
@@ -38,6 +48,7 @@
 			width:770px;
 			height:450px;
 			}
+			
     </style>
 	
     
@@ -52,7 +63,7 @@
 </head>
 <body>
 	<div class="navbar navbar-fixed-top">
-	  <div class="navbar-inner ">
+	  <div class="navbar-inner navbar-inverse ">
 	  <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -74,10 +85,7 @@
 			</div>
 		<!--button group-->
 		<a href="#"><img src="images/logo.png" class="brand" width="150" height="70" id="logo"/></a>
-			<div class="navbar-search input-append pull-right" >
-			  <input class="span2" id="appendedInputButton" type="text" >
-			  <button class="btn" type="button"><i class="icon-search"></i></button>
-			</div>
+			
 		<ul class="nav pull-right nav-pills">
 		  <li class="active"><a href="#"><i class="icon-home"></i> Home</a></li>
 		  <li><a href="#"><i class="icon-user"></i> Profile</a></li>
@@ -85,72 +93,132 @@
 		</ul>
 	  </div>
 	</div>
-	<br>
-	<br>
-	<br>
-	<br>
+	
 <div class="container-fluid">
-	<div class=" span12" id="wrapper">
+	
 			<!--slider-->
-			<div id="myCarousel" class="carousel slide">
-                <div class="carousel-inner">
-                  <div class="item active">
-                    <img src="images/animal1.png" alt="" class="image-content">
-                    <div class="carousel-caption">
-                      <h4>First Thumbnail label</h4>
-                      <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit....<a class="label label-warning">Read More</a></p>
-                    </div>
-                  </div>
-                  <div class="item">
-                    <img src="images/animal2.png" alt="" class="image-content">
-                    <div class="carousel-caption">
-                      <h4>Second Thumbnail label</h4>
-                      <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit...<a class="label label-warning">Read More</a></p>
-                    </div>
-                  </div>
-                  <div class="item">
-                    <img src="images/animal3.png" alt="" class="image-content">
-                    <div class="carousel-caption">
-                      <h4>Third Thumbnail label</h4>
-                      <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit...<a class="label label-warning">Read More</a></p>
-                    </div>
-                  </div>
-                </div>
-                <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-                <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
-              </div>
+			<div class="div2" style="background-color:white;padding:40px 0 50px 20px;border:1px solid #E3E3E3;">
+        <div class="floatLeft">
+            <div id="mcts1">
+                <img src="images/slideshow-1s.jpg" />
+                <img src="images/slideshow-2s.jpg" />
+                <img src="images/slideshow-3s.jpg" />
+                <img src="images/slideshow-4s.jpg" />
+                <img src="images/slideshow-5s.jpg" />
+                <img src="images/slideshow-6s.jpg" />
+                <img src="images/slideshow-7s.jpg" />
+                <img src="images/slideshow-8s.jpg" />
+                <img src="images/slideshow-9s.jpg" />
+                <img src="images/slideshow-10s.jpg" />
+                <img src="images/slideshow-11s.jpg" />
+                <img src="images/slideshow-12s.jpg" />
             </div>
-          </div>
+        </div>
+        <div class="floatLeft">
+            <div id="sliderFrame">
+                <div id="slider">
+                    <img src="images/slider-1.jpg" alt="#slideshow-1" title="Welcome to Menucool jQuery Slideshow" />
+                    <img src="images/slider-2.jpg" alt="#slideshow-2" />
+                    <a href="http://www.menucool.com"><img src="images/slider-3.jpg" alt="#slideshow-3" /></a>
+                    <img src="images/slider-4.jpg" alt="#slideshow-4" />
+                    <img src="images/slider-5.jpg" alt="#slideshow-5" />
+                    <img src="images/slider-6.jpg" alt="#slideshow-6" />
+                    <img src="images/slider-7.jpg" alt="#slideshow-7" />
+                    <img src="images/slider-8.jpg" alt="#slideshow-8" />
+                    <img src="images/slider-9.jpg" alt="#slideshow-9" />
+                    <img src="images/slider-10.jpg"  alt="#slideshow-10"/>
+                    <img src="images/slider-11.jpg"  alt="#slideshow-11"/>
+                    <img src="images/slider-12.jpg"  alt="#slideshow-12"/>
+                </div>
+            </div>
+        </div>
+        <div style="clear:both;"></div>  
+        <div style="display:none;">
+            <div id="slideshow-1">
+                <h3>Welcome to Menucool jQuery Slideshow</h3>
+                This demo shows how the jQuery slideshow (or Thumbnail Slider if using the pure JavaScript) can work together with the JavaScript Image Slider.
+            </div>
+            <div id="slideshow-2">
+                <h3>Enhanced Slideshow Effect</h3>
+                The jQuery Slideshow/thumbnail slider works nicely together with the JavaScript Image Slider which greatly enhanced the slideshow with an added aesthetic appeal.
+            </div>
+            <div id="slideshow-3">
+                <h3>SEO Friendly</h3>                    
+                The markup is valid HTML5 and SEO optimzied, with all content always being available to search engines. 
+            </div>
+            <div id="slideshow-4">
+                <h3>A Tool for Web Designer</h3>
+                Its goal is to simplify the process of creating professional image/content slideshow for the web and mobile devices. This jQuery Slideshow will entertain your audience while scrolling what feature your website has.
+            </div>
+            <div id="slideshow-5">
+                <h3>JavaScript Image Slider is driven by the jQuery/Thumbnail Slideshow</h3>                    
+                Each slide of the JavaScript Image Slider is triggered by the thumbnail scrolling.
+            </div>
+            <div id="slideshow-6">
+                <h3>Rich HTML</h3>                    
+                Rich HTML content can be added to the thumbnails, or to the description of each JavaScript Image Slider slide. 
+            </div>
+            <div id="slideshow-7">
+                <h3>Fully Customizable</h3>                    
+                The jQuery Slideshow, or Thumbnail Slider if using the pure JavaScript, can be horizontal or vertical, and its styles and behaviors are fully customizable by tweaking the CSS and the JavaScipt. 
+            </div>
+            <div id="slideshow-8">
+                <h3>Varying Width, or Varying Height</h3>                    
+                The varying size (either varying width for the horizontal jQuery slideshow, or varying height for the vertical slider) is allowed for the thumbnails. 
+            </div>
+            <div id="slideshow-9">
+                <h3>A Great Way to Showcase Your Products</h3>                    
+                    It is a perfect way to showcase your products. This script allows you to easily animate any series of elements, by sequentially scrolling them.
+            </div>
+            <div id="slideshow-11">
+                <h3>Wide Browsers and Devices Support</h3>                    
+                The carousel is completely configurable and compatible with all major browsers (including ie6+, Firefox Chrome, Opera, Safari) and mobile platforms like iphone / ipad. 
+            </div>
+            <div id="slideshow-12">
+                <h3>A Great Carousel</h3>  
+                Use this stunning carousel to entertain your readers while scrolling what feature your website has. It has a very smooth user interface that gives your slider an added aesthetic appeal. 
+            </div>
+            <div id="slideshow-10">
+                <h3>Easy to Use</h3>                    
+                Enhance your website by adding a unique and attractive slider! And it is easy: download, copy & paster, tweak the styles to your desires. Well, that's it.
+            </div>
+        </div> 
+    </div>
+    
+    </div>
+    
 		  <!--end slider-->
-		
+		 <br> 
+		 <br> 
+		 <br> 
 	<!--thumbnail-->	
 	<div class="container-fluid">
 		<div class="row-fluid">
             <ul class="thumbnails">
               <li class="span3">
                 <div class="thumbnail">
-                  <img src="http://placehold.it/300x200" alt="">
+                  <img src="images/1.jpg" alt="">
 				  <p><a href="#" class="btn btn-primary btn-block btn-large"><i class="icon-file icon-white"></i> JURNAL</a> </p>
                   
                 </div>
               </li>
               <li class="span3">
                 <div class="thumbnail">
-                  <img src="http://placehold.it/300x200" alt="">
+                  <img src="images/2.jpg" alt="">
 				  <p><a href="#" class="btn btn-primary btn-block btn-large"><i class="icon-book icon-white"></i> MODUL</a> </p>
                   
                 </div>
               </li>
               <li class="span3">
                 <div class="thumbnail">
-                  <img src="http://placehold.it/300x200" alt="">
+                  <img src="images/3.jpg" alt="">
 				  <p><a href="#" class="btn btn-primary btn-block btn-large"><i class="icon-book icon-white"></i> BUKU</a> </p>
                   
                 </div>
               </li>
 			  <li class="span3">
                 <div class="thumbnail">
-                  <img src="http://placehold.it/300x200" alt="">
+                  <img src="images/4.jpg" alt="">
 				  <p><a href="#" class="btn btn-primary btn-block btn-large"><i class="icon-file icon-white"></i> BULETIN</a> </p>
                   
                 </div>
@@ -167,6 +235,12 @@
 </div>
  
  
+
+ <script type="text/javascript">
+
+  </script> 
+</body>
+</html>
 
  <script type="text/javascript">
  
