e77c5ab3e396aa370ff86419688841422a20f4ab
fajrul <fajrul.02.a7x@gmail.com>
2012-12-17 02:25:27 +0700

    Halaman Depan Dosen

--- application/views/dosen/index.php
+++ application/views/dosen/index.php
@@ -6,49 +6,154 @@
 	<link href="/third_party/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<link href="/third_party/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
 	<style type="text/css">
+		#logo {
+			height: 31px;
+			margin-top: 0px;
+		}
+		#image {
+			float: left;
+			margin: 0px 15px 10px 0px;
+		}
+
 	</style>
 </head>
 <body>
-	<div class="navbar">
+<div class="navbar navbar-fixed-top">
 		<div class="navbar-inner">
 			<div class="container">
-				<ul class="nav">
-					<li class="active"><a href="#">Beranda</a></li>
-					<li><a href="/dosen/profil">Profil</a></li>
-					<li><a href="#Document">Dokumen</a></li>
-					<li><a href="#Aktivitas">Aktivitas</a></li>
-					<li><a href="#Keluar">Keluar</a></li>
+				<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="/"><img src="/images/gd.png" id="logo" /></a>
+				<ul class="nav pull-right nav-pills">
+					<li><a href="/admin"><i class="icon-home"></i> Dashboard</a></li>
+					<li><a href="#"><i class="icon-user"></i> Profil</a></li>
+				  
 				</ul>
 			</div>
 		</div>
-	</div>
-	<div class="container-fluid">
-		<div class="row-fluid">
-			<div class="well span3">
-				<ul class="nav nav-list">
-					<li class="nav-header"><h4>DOKUMEN</h4></li>
-					<li><a href="#"><i class="icon-book"></i>JURNAL</a></li>
-					<li><a href="#"><i class="icon-book"></i>BUKU</li>
-					<li><a href="#"><i class="icon-book"></i>MODUL</li>
-					<li><a href="#"><i class="icon-book"></i>BULETTIN</li>
-				<li class="divider"></li>
-				......
+</div>
+<br />
+<br />
+<br />
+<div class="container-fluid">
+	<div class="row-fluid">
+		<div class="span2">
+			<div class="well">
+				<ul class="thumbnails">
+					<li class="span12">
+						<a href="#" class="thumbnail"><img src="/images/D_oS.png" alt=""></a>
+					</li>
+				</ul>
+				<span>
+					<strong>Administrator</strong><br>
+					<a href="#Profil"> Dosen </a><br>
+					<a href="#Setting">Settings</a> | <a href="#">Keluar</a>
+				</span>
+			</div>
+			<div class="well">
+			<div class="sidebar-nav">
+				<div>
+					<ul class="nav nav-tabs nav-stacked">
+						<li class="nav-header"><h3>Dokumen</h3></li>
+						<li><a href="#"><i class="icon-file"></i> JURNAL</a></li>
+						<li><a href="#"><i class="icon-file"></i> BUKU</a></li>
+						<li><a href="#"><i class="icon-file"></i> MODUL</a></li>
+						<li><a href="#"><i class="icon-file"></i> BULETIN</a></li>
+						<li class="divider"></li>
+					</ul>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="span10">
+			<div>
+				<ul class="breadcrumb">
+				<li><a href="/">Home</a> <span class="divider">/</span></li>
+				<li><a href="#">Dosen</a> <span class="divider">/</span></li>
+				<li class="active">Data</li>
 				</ul>
 			</div>
-			<div class="well span9">
+			<div class="row-fluid">
+				<div class="span10">
+					<table class="table">
+						<thead>
+							<tr>
+							  <th>JUDUL</th>
+							  <th>TANGGAL</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+							  <td>Jaringan Komputer</td>
+							  <td>12/12/12</td>
+							  <td>
+							  <button class="btn btn-danger btn-mini"><i class="icon-trash icon-white"></i></button> &nbsp;
+							  <button class="btn btn-warning btn-mini"><i class="icon-edit icon-white"></i></button> &nbsp;
+							  <button class="btn btn-success btn-mini"><i class="icon-share-alt icon-white"></i></button>
+							  </td>
+							</tr>
+							<tr>
+							  <td>Mahir HTML dan CSS</td>
+							  <td>12/12/12</td>
+							  <td>
+							  <button class="btn btn-danger btn-mini"><i class="icon-trash icon-white"></i></button>&nbsp;
+							  <button class="btn btn-warning btn-mini"><i class="icon-edit icon-white"></i></button>&nbsp;
+							  <button class="btn btn-success btn-mini"><i class="icon-share-alt icon-white"></i></button>
+							  </td>
+							</tr>
+							<tr>
+							  <td>Mahir PHP dan Data Base</td>
+							  <td>12/12/12</td>
+							  <td>
+							  <button class="btn btn-danger btn-mini"><i class="icon-trash icon-white"></i></button>&nbsp;
+							  <button class="btn btn-warning btn-mini"><i class="icon-edit icon-white"></i></button>&nbsp;
+							  <button class="btn btn-success btn-mini"><i class="icon-share-alt icon-white"></i></button>
+							  </td>
+							</tr><tr>
+							  <td>Struktur Data</td>
+							  <td>12/12/12</td>
+							  <td>
+							  <button class="btn btn-danger btn-mini"><i class="icon-trash icon-white"></i></button>&nbsp;
+							  <button class="btn btn-warning btn-mini"><i class="icon-edit icon-white"></i></button>&nbsp;
+							  <button class="btn btn-success btn-mini"><i class="icon-share-alt icon-white"></i></button>
+							  </td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 				<div class="row-fluid">
-					<div class="well span4">
-						<img src="/images/animal6.png" alt="animal3.png" />
+					<div class="span12">
+						<div class="well span6">
+							<a href="#Doc"><img src="/images/doc.jpg" width="105" height="97" id="image" /><a/><h5>JUDUL DOKUMEN</h5><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet...<br />
+							<a href="#Lihat_Doc"><i class="icon-eye-open"></i>Lihat</a> | <a href="#Edit_Doc"><i class="icon-edit"></i>Edit</a> | <a href="#"><i class="icon-trash"></i>Delete</a>
+						</div>
+						<div class="well span6">
+							<a href="#Doc"><img src="/images/doc.jpg" width="105" height="97" id="image" /><a/><h5>JUDUL DOKUMEN</h5><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet...<br />
+							<a href="#Lihat_Doc"><i class="icon-eye-open"></i>Lihat</a> | <a href="#Edit_Doc"><i class="icon-edit"></i>Edit</a> | <a href="#"><i class="icon-trash"></i>Delete</a>
+						</div>
 					</div>
-					<div class="well span8">
-						<h4>JUDUL DOKUMEN</h4>
-						<p>rtdtdjyfjjshflaidhjfladjflakdfb jlakdjfskdfhlakdfhae uhajsfnjsdlkajhdfjahdfkasjdhfkja dsfkaljdfhasjdhfaskdjfhaksdjfhasdjkfhhasdjfbasdjh fbashjfbasdhfa</p>
+				</div>
+				<div class="row-fluid">
+					<div class="span12">
+						<div class="well span6">
+							<a href="#Doc"><img src="/images/doc.jpg" width="105" height="97" id="image" /><a/><h5>JUDUL DOKUMEN</h5><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet...<br />
+							<a href="#Lihat_Doc"><i class="icon-eye-open"></i>Lihat</a> | <a href="#Edit_Doc"><i class="icon-edit"></i>Edit</a> | <a href="#"><i class="icon-trash"></i>Delete</a>
+						</div>
+						<div class="well span6">
+							<a href="#Doc"><img src="/images/doc.jpg" width="105" height="97" id="image" /><a/><h5>JUDUL DOKUMEN</h5><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet...<br />
+							<a href="#Lihat_Doc"><i class="icon-eye-open"></i>Lihat</a> | <a href="#Edit_Doc"><i class="icon-edit"></i>Edit</a> | <a href="#"><i class="icon-trash"></i>Delete</a>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<script type="text/javascript" src="/third_party/jquery/jquery-1.8.2.min.js"></script>
-	<script type="text/javascript" src="/third_party/bootstrap/bootstrap.min.js"></script>
+</div>
+
 </body>
+	<script src="/third_party/jquery.ui/jquery-1.8.2.js"></script>
+	<script src="/third_party/jquery.ui/jquery-ui-1.9.1.custom.min.js"></script>
+	<script src="/third_party/bootstrap/bootstrap.min.js"></script>
 </html>
