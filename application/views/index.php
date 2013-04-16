54a876c0cb3f0d625993e86c7a21d36acfa8b4d2
Safana <safana.bib@gmail.com>
2013-04-16 19:16:09 +0700

    hilang semua lah

--- application/views/index.php
+++ application/views/index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 	<title>GDLib :: G{edung}D Library</title>
 	
 	<link rel="stylesheet" href="/third_party/bootstrap/css/elemento.min.css" />
